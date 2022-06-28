ba637083502d6785644267ab9409654989e76a48
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-06-27 22:40:24 -0300

    chore: finalize favorites page

--- src/templates/Favorites/index.tsx
+++ src/templates/Favorites/index.tsx
@@ -1,6 +1,8 @@
 import Breadcrumb from 'components/Breadcrumb'
 import Container from 'components/Container'
 import Image from 'next/image'
+import Link from 'next/link'
+import { IoStar } from 'react-icons/io5'
 import * as S from './styles'
 
 const Favorites = () => {
@@ -16,14 +18,48 @@ const Favorites = () => {
           center={true}
         />
         <h1>Favorites</h1>
-        <S.FavoritesList>
+        <S.FavoritesContent>
           <h2>Favorites</h2>
           <S.FavoritesHeader>
             <h3>Professional Info</h3>
             <h3>Actions</h3>
           </S.FavoritesHeader>
-          <S.FavoritesItem></S.FavoritesItem>
-        </S.FavoritesList>
+          <S.FavoritesItem>
+            <S.FavoritesIcon>
+              <button type="button">
+                <Image
+                  src="/images/fill-fav.svg"
+                  alt="Favoritar"
+                  width={34}
+                  height={34}
+                  quality={100}
+                />
+              </button>
+            </S.FavoritesIcon>
+            <S.FavoritesData>
+              <div>
+                <img src="/images/Favorites/profile.png" alt="Profile" />
+              </div>
+              <div>
+                <h3>Maria Caroline</h3>
+                <p>
+                  <span>
+                    <IoStar />
+                  </span>
+                  4.5
+                </p>
+              </div>
+            </S.FavoritesData>
+            <S.FavoriteLinks>
+              <Link href="/chat">Chat</Link>
+              <Link href="/profile/1" passHref>
+                <a>
+                  <span>See profile</span>
+                </a>
+              </Link>
+            </S.FavoriteLinks>
+          </S.FavoritesItem>
+        </S.FavoritesContent>
       </Container>
     </S.Main>
   )
--- src/templates/Favorites/styles.ts
+++ src/templates/Favorites/styles.ts
@@ -21,7 +21,7 @@ export const Main = styled.main`
   }
 `
 
-export const FavoritesList = styled.nav`
+export const FavoritesContent = styled.div`
   h2 {
     color: var(--blackTwo);
     font-family: var(--title);
@@ -29,6 +29,10 @@ export const FavoritesList = styled.nav`
     font-size: 35px;
     line-height: 44px;
     margin-bottom: 41px;
+
+    @media (max-width: 767px) {
+      display: none;
+    }
   }
 `
 
@@ -48,14 +52,151 @@ export const FavoritesHeader = styled.div`
     font-size: 20px;
     line-height: 25px;
   }
+
+  @media (max-width: 767px) {
+    display: none;
+  }
 `
 
-export const FavoritesItem = styled.a`
+export const FavoritesList = styled.ul`
+  display: grid;
+  gap: 45px;
+`
+
+export const FavoritesItem = styled.li`
   display: block;
   padding: 30px 20px;
   border-radius: 15px;
   background: var(--white);
   display: grid;
-  grid-template-columns: auto 1fr auto auto;
+  grid-template-columns: auto 1fr;
   gap: 14px;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    grid-template-columns: auto 1fr auto;
+  }
+`
+
+export const FavoritesIcon = styled.div`
+  justify-content: center;
+
+  button {
+    width: 34px;
+    height: 34px;
+    padding: 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    background: none;
+    outline: 0px;
+    cursor: pointer;
+  }
+`
+
+export const FavoritesData = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  div {
+    &:first-child {
+      width: 63px;
+      height: 63px;
+      position: relative;
+      overflow: hidden;
+      border-radius: 50%;
+    }
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  h3 {
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 32px;
+    letter-spacing: -0.02em;
+    color: var(--blackTwo);
+  }
+
+  p {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    span {
+      font-size: 24px;
+    }
+
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 32px;
+    letter-spacing: -0.02em;
+    color: var(--blackTwo);
+  }
+`
+
+export const FavoriteLinks = styled.nav`
+  display: grid;
+  align-items: center;
+  gap: 10px 30px;
+
+  @media (min-width: 576px) {
+    display: flex;
+    width: max-content;
+  }
+
+  @media (max-width: 767px) {
+    grid-column: span 2;
+  }
+
+  a {
+    width: 100%;
+    text-align: center;
+
+    @media (min-width: 576px) {
+      width: 147px;
+    }
+
+    &:nth-child(1) {
+      font-weight: 700;
+      font-size: 18px;
+      line-height: 32px;
+      letter-spacing: -0.02em;
+      color: var(--white);
+      background: linear-gradient(251.43deg, #c28cde 3.15%, #884acc 97.02%)
+          padding-box,
+        linear-gradient(251.43deg, #c28cde 3.15%, #884acc 97.02%) border-box;
+      border-radius: 60px;
+      display: block;
+      padding: 2px 10px;
+      border: 2px solid transparent;
+    }
+
+    &:nth-child(2) {
+      border-radius: 60px;
+      display: block;
+      padding: 2px 10px;
+      border: 2px solid transparent;
+      background: linear-gradient(#fff, #fff) padding-box,
+        linear-gradient(251.43deg, #c28cde 3.15%, #884acc 97.02%) border-box;
+
+      span {
+        background: linear-gradient(251.43deg, #c28cde 3.15%, #884acc 97.02%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 32px;
+        letter-spacing: -0.02em;
+      }
+    }
+  }
 `
